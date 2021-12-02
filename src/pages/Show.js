fc0c698d9caa6f3d3ed1a322498b1a4eb3fb0109
Aline Silva <lisie_su@yahoo.com.br>
2021-12-02 16:51:04 +0000

    Added single show page info

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,10 +1,56 @@
+import { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import ShowsContext from "../context/context";
+
 function Show() {
-    return (
-      <div>
-        <h2>TV Show</h2>
-      </div>
-    );
+  const { getSingleShow, singleShow } = useContext(ShowsContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    getSingleShow(id);
+  }, []);
+
+  function removeTags(text) {
+    text = text.toString();
+    return text.replace(/(<([^>]+)>)/gi, "");
   }
-  
-  export default Show;
-  
+
+  function getYear(date) {
+    let newDate = new Date(date);
+    return newDate.getFullYear();
+  }
+
+  return (
+    <div>
+      <div className="show">
+        <img
+          src={
+            singleShow.image
+              ? singleShow.image.medium
+              : "https://safetyaustraliagroup.com.au/wp-content/uploads/2019/05/image-not-found.png"
+          }
+          alt={singleShow.name}
+        />
+        <div className="info">
+          <h1>
+            {singleShow.name}{" "}
+            {singleShow.premiered && getYear(singleShow.premiered)}
+          </h1>
+          <p>{singleShow.summary && removeTags(singleShow.summary)}</p>
+          <p>
+            <strong>Status:</strong> {singleShow.status && singleShow.status}{" "}
+          </p>
+          <p>
+            <strong>Genres:</strong>
+            {singleShow.genres &&
+              singleShow.genres.map((genre) => (
+                <span key={genre}>{genre}</span>
+              ))}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Show;
